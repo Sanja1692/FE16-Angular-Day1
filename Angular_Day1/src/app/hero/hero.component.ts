e1016435aeb5548584603d905acd99419077ff36
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-06-13 15:20:33 +0200

    contact

--- Angular_Day1/src/app/hero/hero.component.ts
+++ Angular_Day1/src/app/hero/hero.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent implements OnInit {
+display: string = "We are your ultimate";
+display1: string = "Winter experience Provider";
+display2: string = "With hundreds of winter holiday destinations offered, we are your <br> natural choice for planning it all out!";
 
   constructor() { }
 
--- Angular_Day1/src/app/home-page/home-page.component.ts
+++ Angular_Day1/src/app/home-page/home-page.component.ts
@@ -18,14 +18,20 @@ fourP: string = "Our travel agents are always ready to plan your project trip!";
 natpis: string = "Best Winter Trips";
 pics1: string = "https://cdn.pixabay.com/photo/2019/12/06/05/46/alpine-village-4676673_960_720.jpg";
 pics2: string = "https://cdn.pixabay.com/photo/2016/02/07/19/48/aurora-1185464_960_720.jpg";
-
 pics3: string = "https://cdn.pixabay.com/photo/2019/09/03/11/15/hiking-trails-4449183_960_720.jpg";
-
 pics4: string = "https://cdn.pixabay.com/photo/2012/03/01/15/58/active-20502_960_720.jpg";
-
 pics5: string = "https://cdn.pixabay.com/photo/2017/08/14/17/32/switzerland-2641252_960_720.jpg";
-
 pics6: string = "https://cdn.pixabay.com/photo/2022/01/10/12/06/hotel-6928165_960_720.jpg";
+dest1: string = "Jasna, Slovakia";
+dest2: string = "Abisko,Sweden";
+
+dest3: string = "Lapland,Finland";
+
+dest4: string = "Neuschneher, Austria";
+
+dest5: string = "Abisko,Sweden";
+dest6: string = "Jasna, Slovakia";
+
 
   constructor() { }
 
