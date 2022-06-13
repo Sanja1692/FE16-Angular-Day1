c129ea127e2624f12e74d7311c639a942523a4d3
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-06-13 14:45:37 +0200

    routing

--- Angular_Day1/src/app/home-page/home-page.component.ts
+++ Angular_Day1/src/app/home-page/home-page.component.ts
@@ -6,7 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
+title: string = "Why We're the Best";
+first: string = "Accommondation";
+firstP: string = "We offer the first selection of top match hotels anywhere!";
+second: string = "Transportation";
+secondP: string = "Whether it's about air tickets or shuttle bus, we will set it up!";
+third: string = "Seasoned Agents";
+thirdP: string = "Combined, the experience our agents have is centuries!";
+four: string = "Easy Trip Planning";
+fourP: string = "Our travel agents are always ready to plan your project trip!";
   constructor() { }
 
   ngOnInit(): void {
