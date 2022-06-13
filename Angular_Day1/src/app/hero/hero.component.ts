7bf6292084af81e4af5ef105edc14485dde8fd32
Sanja Kostantinovic <sanja.1692@gmail.com>
2022-06-13 15:52:49 +0200

    stop reloading

--- Angular_Day1/src/app/hero/hero.component.ts
+++ Angular_Day1/src/app/hero/hero.component.ts
@@ -9,7 +9,7 @@ export class HeroComponent implements OnInit {
 display: string = "We are your ultimate";
 display1: string = "Winter experience Provider";
 display2: string = "With hundreds of winter holiday destinations offered, we are your <br> natural choice for planning it all out!";
-
+btnText: string = "Learn more";
   constructor() { }
 
   ngOnInit(): void {
